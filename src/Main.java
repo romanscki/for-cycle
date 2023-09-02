0e27677002a390a5bce7a0bdc0d28e4d7dbfa30c
Roman Giren <romanscki@gmail.com>
2023-09-02 16:01:04 +0300

    practic

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+
+        for (int a = 0; a <= value; a = a + 1) {
+            for (int b = 1; b <= value; b = b + 1) {
+                if (a * b == value) {
+                    System.out.println(a + "*" + b);
+                }
+            }
+        }
+    }
+}
